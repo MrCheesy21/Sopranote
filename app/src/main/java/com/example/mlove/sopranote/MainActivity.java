cfdf3eed7dc754b22db1cec75b5eca57280e390f
Mustafa Hussain <mustafa.hussain2357@gmail.com>
2018-10-15 13:24:57 -0400

    basic calculator?

--- app/src/main/java/com/example/mlove/sopranote/MainActivity.java
+++ app/src/main/java/com/example/mlove/sopranote/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.mlove.sopranote;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText numOne = (EditText) findViewById(R.id.numOne);
+        EditText numTwo = (EditText) findViewById(R.id.numTwo);
+        Button Addbtn = (Button) findViewById(R.id.Addbtn);
+        TextView result = (TextView) findViewById(R.id.result);
+        Addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int num1 = Integer.parseInt(numOne.getText().toString());
+                int num2 = Integer.parseInt(numTwo.getText().toString());
+                int added = num1 + num2;
+                result.setText(added + "");
+
+            }
+        });
     }
 }
