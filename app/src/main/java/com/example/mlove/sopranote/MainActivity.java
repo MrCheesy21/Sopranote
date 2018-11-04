40da886f4a3acf47c0ca27801f0eb08c86c644d2
Mustafa Hussain <mustafa.hussain2357@gmail.com>
2018-11-03 22:18:05 -0400

    use basic pitch detection functionality with TarsosDSP

--- app/src/main/java/com/example/mlove/sopranote/MainActivity.java
+++ app/src/main/java/com/example/mlove/sopranote/MainActivity.java
@@ -1,21 +1,27 @@
 package com.example.mlove.sopranote;
 
-import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button recordBtn, stopBtn, playBtn;
     private MediaRecorder recorder;
-    private String outputFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,25 +29,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         recordBtn = (Button) findViewById(R.id.record);
         stopBtn = (Button) findViewById(R.id.stop);
-        playBtn = (Button) findViewById(R.id.play);
         stopBtn.setEnabled(false);
         playBtn.setEnabled(false);
-        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
         recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-        recorder.setOutputFile(outputFile);
-
-
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult result, AudioEvent e) {
+                final float pitchInHz = result.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView text = (TextView) findViewById(R.id.pitch);
+                        text.setText("" + pitchInHz);
+                    }
+                });
+            }
+        };
+        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050, 1024, pdh);
+        dispatcher.addAudioProcessor(p);
+        new Thread(dispatcher,"Audio Dispatcher").start();
         recordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+                recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+                recorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+                recorder.setOutputFile("/dev/null");
                 try {
                     recorder.prepare();
                     recorder.start();
-                } catch (IllegalStateException | IOException ise) {
+                } catch (IllegalStateException | IOException e) {
                     Toast.makeText(getApplicationContext(), "error, please try again", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
                 }
                 recordBtn.setEnabled(false);
                 stopBtn.setEnabled(true);
@@ -52,30 +72,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 recorder.stop();
-                recorder.release();
-                recorder = null;
+                recorder.reset();
+                //recorder.release();
+                //recorder = null;
                 stopBtn.setEnabled(false);
                 recordBtn.setEnabled(true);
                 playBtn.setEnabled(true);
-                Toast.makeText(getApplicationContext(), "recording stopped", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "recording stopped, maxAmp = ", Toast.LENGTH_LONG).show();
             }
         });
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                    try {
-                        mediaPlayer.setDataSource(outputFile);
-                        mediaPlayer.prepare();
-                        mediaPlayer.start();
-                        Toast.makeText(getApplicationContext(), "playing..", Toast.LENGTH_LONG).show();
-                    } catch (Exception e) {
-                        Toast.makeText(getApplicationContext(), "error, please try again", Toast.LENGTH_LONG).show();
-                    }
-                }
-            });
-        }
     }
+}
 
 
 
