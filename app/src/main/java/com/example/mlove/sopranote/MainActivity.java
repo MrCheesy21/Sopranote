73551e48ce4676fc21d420a421c8b5a2cc4917a5
Mustafa Hussain <mustafa.hussain2357@gmail.com>
2019-01-18 10:27:41 -0500

    change algorithm to include chromatic notes in between C scale

--- app/src/main/java/com/example/mlove/sopranote/MainActivity.java
+++ app/src/main/java/com/example/mlove/sopranote/MainActivity.java
@@ -19,15 +19,14 @@ public class MainActivity extends AppCompatActivity {
 
     TextView note, pitch;
     ImageView A, B, C,  D, E, F, G;
-    private static final double BASE = 440.0;
-    private final String[] notes = {"A", "A", "B", "C", "C", "D", "D", "E", "F", "F", "G", "G"};
+    private final String[] noteVals = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
     private ImageView[] noteImages;
     private ImageView tempImage;
-    private static final String[] allNotes = new String[5001];
-    private static final String[] newNotes = new String[5001];
-    private static double previous = 41;
-    private static double current = 41;
-    private static int counter = -40;
+    private static final String[] notes = new String[5001];
+    private static double range = 1.225;
+    private static int cursor = 7;
+    private static double index = 41;
+    private static double ind = 41;
 
 
     @Override
@@ -46,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         for (ImageView e: noteImages) {
             e.setVisibility(View.GONE);
         }
-        shiftPitches();
+        shiftPitches(noteVals);
         pitch = findViewById(R.id.txtFrequency);
         note = findViewById(R.id.txtNote);
         AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050,1024,0);
@@ -70,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
     public void processPitch(float pitchInHz) {
         if (pitchInHz != -1.0) {
             pitch.setText(String.format("%.2f", pitchInHz));
-            note.setText(newNotes[(int) pitchInHz]);
+            note.setText(notes[(int) pitchInHz]);
             try {
                 Thread.sleep(30);
             } catch (InterruptedException e) {
@@ -79,6 +78,13 @@ public class MainActivity extends AppCompatActivity {
 
             for (ImageView e: noteImages) {
                 if (findViewById(R.id.A) == e &&  note.getText().equals("A")) {
+                    if (tempImage != e) {
+                        tempImage.setVisibility(View.INVISIBLE);
+                        e.setVisibility(View.VISIBLE);
+                        tempImage = e;
+                    }
+                }
+                if (findViewById(R.id.A) == e &&  note.getText().equals("A#")) {
                     if (tempImage != e) {
                         tempImage.setVisibility(View.INVISIBLE);
                         e.setVisibility(View.VISIBLE);
@@ -99,7 +105,21 @@ public class MainActivity extends AppCompatActivity {
                         tempImage = e;
                     }
                 }
+                if (findViewById(R.id.C) == e &&  note.getText().equals("C#")) {
+                    if (tempImage != e) {
+                        tempImage.setVisibility(View.INVISIBLE);
+                        e.setVisibility(View.VISIBLE);
+                        tempImage = e;
+                    }
+                }
                 if (findViewById(R.id.D) == e &&  note.getText().equals("D")) {
+                    if (tempImage != e) {
+                        tempImage.setVisibility(View.INVISIBLE);
+                        e.setVisibility(View.VISIBLE);
+                        tempImage = e;
+                    }
+                }
+                if (findViewById(R.id.D) == e &&  note.getText().equals("D#")) {
                     if (tempImage != e) {
                         tempImage.setVisibility(View.INVISIBLE);
                         e.setVisibility(View.VISIBLE);
@@ -120,7 +140,23 @@ public class MainActivity extends AppCompatActivity {
                         tempImage = e;
                     }
                 }
+                if (findViewById(R.id.F) == e &&  note.getText().equals("F#")) {
+                    if (tempImage != e) {
+                        tempImage.setVisibility(View.INVISIBLE);
+                        e.setVisibility(View.VISIBLE);
+                        tempImage = e;
+                    }
+                }
                 if (findViewById(R.id.G) == e &&  note.getText().equals("G")) {
+
+                    if (tempImage != e) {
+                        tempImage.setVisibility(View.INVISIBLE);
+                        e.setVisibility(View.VISIBLE);
+                        tempImage = e;
+                    }
+                }
+                if (findViewById(R.id.G) == e &&  note.getText().equals("G#")) {
+
                     if (tempImage != e) {
                         tempImage.setVisibility(View.INVISIBLE);
                         e.setVisibility(View.VISIBLE);
@@ -131,74 +167,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void shiftPitches() {
-        for (int i = 40; i <= 5000; i++) {
-            float temp = Math.abs(i);
-            int index = Math.abs(determineFreq(temp));
-            int tempIndex = index % 12;
-            if (i < BASE) {
-                tempIndex = 11 - tempIndex;
+    public void shiftPitches(String[] noteVals) {
+            while (index < notes.length) {
+                while (ind < index + range) {
+                    if (ind < notes.length) {
+                        notes[(int) ind] =  noteVals[cursor % 12];
+                    }
+                    ind++;
+                }
+                cursor++;
+                index += range * 2;
+                nextRange();
             }
-            String tempNote = notes[tempIndex];
-            allNotes[i] = tempNote;
-        }
-        for (int i = 40; i <= 5000; i++) {
-            boolean special = false;
-            if (i < (int)  (getCurrent())) {
-                newNotes[i] = allNotes[i];
-            } else if (i == (int)  (getCurrent())) {
-                getNextFreq(counter++);
-                int temp = (int)  (getCurrent());
-                if (temp <= 1000) {
-                    if (allNotes[(int)((getCurrent() - getPrevious()) / 2.0 + getPrevious())].equals("B")
-                            || allNotes[(int)((getCurrent() - getPrevious()) / 2.0 + getPrevious())].equals("E")) {
-                        temp = (int) ((getCurrent() - getPrevious()) / 2.0 + getPrevious());
-                        special = true;
-                    }
-                }
-                int holder = temp;
-                while (i < holder) {
-                    if (temp > 1000) {
-                        temp = 999;
-                        holder = temp;
-                    }
-                    if (special) {
-                        while (allNotes[temp].equals("B") || allNotes[temp].equals("E")) {
-                            temp++;
-                        }
-                        special = false;
-                    }
-                    newNotes[i] = allNotes[holder + 1];
-                    i++;
-                }
-                while (i < temp - 1) {
-                    newNotes[i] = allNotes[temp];
-                    i++;
-                }
-                i--;
-            }
-        }
     }
 
-    private static int determineFreq(float pitch) {
-        return (int) (12 * log(pitch/BASE, 2));
+
+    private static void nextRange() {
+        range *= Math.cbrt(Math.sqrt(Math.sqrt(2)));
     }
 
-    private static double log(double x, double base) {
-        return Math.log(x)/Math.log(base);
-    }
-
-    private static void getNextFreq(int index) {
-        previous = current;
-        current = 440.0 * Math.pow(2, ((index + 1.0) / 12.0));
-    }
-
-    private static double getCurrent() {
-        return current;
-    }
-
-    private static double getPrevious() {
-        return previous;
-    }
 }
 
